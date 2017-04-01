c3e244a1f8a6a8a663390d37fd60ea84b55c26ca
Eddie <adward_r@icloud.com>
2017-04-01 00:46:14 -0400

    ps4 - serial update 1

--- ps4/serial.c
+++ ps4/serial.c
@@ -9,83 +9,94 @@
 #define TIME_INTERVAL 128
 #define G 1.0
 #define N_DIM 3
+#define CUTOFF_SQR 25.0
+#define EPSILON 0.0001
 
-void readdata(char filename[], int *N, int *K, double *DT, 
-		double *mass, double (*x)[N_DIM], double (*v)[N_DIM]) {
-	FILE *fp;
+typedef struct body_t {
+	double mass;
+	double x[N_DIM];
+	double v[N_DIM];
+} Body;
+
+void readdata(int *N, int *K, double *DT, Body bds[]) {
 	int i;
 
-	if ((fp = fopen(filename, "r")) == NULL) {
-		printf("Data cannot be read!\n");
-		exit(1);
-	}
-	fscanf(fp, "%d", N);
-	fscanf(fp, "%d", K);
-	fscanf(fp, "%lf", DT);
-	for (i = 0; i < *N; ++ i) fscanf(fp, "%lf", mass+i);
-	for (i = 0; i < *N; ++ i) fscanf(fp, "%lf %lf %lf", &x[i][0], &x[i][1], &x[i][2]);
-	for (i = 0; i < *N; ++ i) fscanf(fp, "%lf %lf %lf", &v[i][0], &v[i][1], &v[i][2]);
-	fclose(fp);
+	scanf("%d", N);
+	scanf("%d", K);
+	scanf("%lf", DT);
+	for (i = 0; i < *N; ++ i) scanf("%lf", &bds[i].mass);
+	for (i = 0; i < *N; ++ i) scanf("%lf %lf %lf", &(bds[i].x)[0], &(bds[i].x)[1], &(bds[i].x)[2]);
+	for (i = 0; i < *N; ++ i) scanf("%lf %lf %lf", &(bds[i].v)[0], &(bds[i].v)[1], &(bds[i].v)[2]);
 }
 
-void report(int t, int N, double DT, double mass[], double (*x)[N_DIM], double (*v)[N_DIM]) {
-	double cx[N_DIM], cv[N_DIM];
-	double msum = .0;
+Body get_centroid(int n_body, Body bodies[]) {
 	int i, k;
-
-	for (k = 0; k < N_DIM; ++ k) cx[k] = cv[k] = .0;
-	for (i = 0; i < N; ++ i) {
-		msum += mass[i];
+	Body centroid;
+	centroid.mass = .0;
+	for (k = 0; k < N_DIM; ++ k) centroid.x[k] = centroid.v[k] = .0;
+	for (i = 0; i < n_body; ++ i) {
+		centroid.mass += bodies[i].mass;
 		for (k = 0; k < N_DIM; ++ k) {
-			cx[k] += mass[i] * x[i][k];
-			cv[k] += v[i][k];
+			centroid.x[k] += bodies[i].mass * bodies[i].x[k];
+			centroid.v[k] += bodies[i].v[k];
 		}
 	}
 	for (k = 0; k < N_DIM; ++ k) {
-		// printf("cx[%d] = %lf\n", k, cx[k]);
-		cx[k] /= msum;
-		cv[k] /= N;
+		centroid.x[k] /= centroid.mass;
+		centroid.v[k] /= n_body;
+		centroid.mass /= n_body;
 	}
+	return centroid;
+}
 
+void compute_force_as_vector(const Body *this, const Body *that, double vec[]) {  // no init happens inside
+	double r2, tmp;
+	int k;
+	
+	if (this == that) return;
+	r2 = pow(this->x[0] - that->x[0], 2) + pow(this->x[1] - that->x[1], 2) + pow(this->x[2] - that->x[2], 2);
+	if (r2 > CUTOFF_SQR) return;
+	tmp = (G * this->mass * that->mass) / pow(r2, 1.5);  // quantative variable
+	for (k = 0; k < N_DIM; ++ k) vec[k] += tmp * (that->x[k] - this->x[k]);  // vector variable
+}
+
+void report(int t, double DT, const Body* c) {
 	printf("\nConditions after timestep %d (time = %lf) :\n\n", t, t * DT);
-	printf("\tCenter of Mass:\t(%lf, %lf, %lf)\n", cx[0], cx[1], cx[2]);
-	printf("\tAverage Velocity:\t(%lf, %lf, %lf)\n", cv[0], cv[1], cv[2]);
+	printf("\tCenter of Mass:\t(%lf, %lf, %lf)\n", c->x[0], c->x[1], c->x[2]);
+	printf("\tAverage Velocity:\t(%lf, %lf, %lf)\n", c->v[0], c->v[1], c->v[2]);
 }
 
 int main(int argc, char **argv) {
 	int N;  // number of bodies
 	int K;  // number of time steps
 	double DT;  // time step size
-	double mass[MAX_N_BODY];
-	double x[MAX_N_BODY][N_DIM];  // position in each dimension
-	double v[MAX_N_BODY][N_DIM];  // velocity in each dimension
 	
-	double a[N_DIM];  // acceleration in each dimension
 	double F[N_DIM];  // force in each dimension
 	int i, j, k, t;
-	double tmp, r2;
 
-	readdata("data/testdata1", &N, &K, &DT, mass, x, v);
-	// printf("%d %d %lf\n", N, K, DT);
-	// for (i = 0; i < N; ++ i) printf("%lf %lf %lf\n", x[i][0], x[i][1], x[i][2]);
+	Body bodies[MAX_N_BODY];
+	Body centroid;
+
+	readdata(&N, &K, &DT, bodies);
+	// printf("%lf %lf %lf\n", bodies[0].x[0], bodies[0].x[1], bodies[0].x[2]);
 
 	for (t = 0; t < K; ++ t) {
-		if (t % TIME_INTERVAL == 0) report(t, N, DT, mass, x, v);
+		if (t % TIME_INTERVAL == 0) {
+			centroid = get_centroid(N, bodies);
+			report(t, DT, &centroid);
+		}
 		for (i = 0; i < N; ++ i) {  // for each body
 			F[0] = F[1] = F[2] = .0;
-			for (j = 0; j < N; ++ j) {
-				if (j == i) continue;
-				r2 = pow(x[i][0] - x[j][0], 2) + pow(x[i][1] - x[j][1], 2) + pow(x[i][2] - x[j][2], 2);
-				if (r2 > 25.0) continue;
-				tmp = (G * mass[i] * mass[j]) / (r2 * sqrt(r2));  // quantative variable
-				for (k = 0; k < 3; ++ k) F[k] += tmp * (x[j][k] - x[i][k]);  // vector variable
+			for (j = 0; j < N; ++ j) 
+				compute_force_as_vector(&bodies[i], &bodies[j], F);  // accumulates vec variable F
+			for (k = 0; k < N_DIM; ++ k) {
+				bodies[i].v[k] += F[k] * (DT / 2.0) / bodies[i].mass;
+				bodies[i].x[k] += bodies[i].v[k] * DT; 
 			}
-			for (k = 0; k < 3; ++ k) a[k] = F[k] / mass[k];
-			for (k = 0; k < 3; ++ k) v[i][k] += a[k] * (DT / 2.0);
-			for (k = 0; k < 3; ++ k) x[i][k] += v[i][k] * DT;
 		}
 	}
-	report(K, N, DT, mass, x, v);
+	centroid = get_centroid(N, bodies);
+	report(t, DT, &centroid);
 
 	return 0;
 }
