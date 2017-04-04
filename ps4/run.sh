69eb247809b3cf9edf51756154c6187b5494d4f7
Keyang Dong of group cpsc424 <kd538@compute-33-1.local>
2017-04-04 02:31:34 -0400

    Script files

--- ps4/run.sh
+++ ps4/run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-#PBS -l procs=1,tpn=1,mem=34gb,walltime=30:00
-#PBS -q cpsc424
+#PBS -l procs=8,tpn=2,mem=34gb,walltime=30:00
+#PBS -N fserial
+#PBS -r n
+#PBS -q fas_devel
 #PBS -j oe
 
+module load Langs/Intel/15 MPI/OpenMPI/1.8.6-intel15
+pwd
 cd $PBS_O_WORKDIR
+pwd
+make clean
+make
 
-module load Langs/Intel/15
+# mpiexec -n 8 ./fserial < ./data/testdata1 > ./testdata1_f.out
+# mpiexec -n 8 ./fserial < ./data/testdata2 > ./testdata2_f.out
 
-./serial < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/testdata1 > ./testdata1_c.out
-
-#./fserial < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/testdata1 > ./testdata1_f.out
+mpiexec -n 8 ./fserial < ./data/actualdata1 > ./actualdata1_f.out
+mpiexec -n 8 ./fserial < ./data/actualdata2 > ./actualdata2_f.out
+mpiexec -n 8 ./fserial < ./data/actualdata3 > ./actualdata3_f.out
+mpiexec -n 8 ./fserial < ./data/actualdata4 > ./actualdata4_f.out
---
+++ ps4/run_serial.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l procs=1,tpn=1,mem=34gb,walltime=30:00
+#PBS -N serial
+#PBS -r n
+#PBS -q fas_devel
+#PBS -j oe
+
+module load Langs/Intel/15 MPI/OpenMPI/1.8.6-intel15
+pwd
+cd $PBS_O_WORKDIR
+pwd
+make clean
+make
+
+# mpiexec -n 8 ./fserial < ./data/testdata1 > ./testdata1_f.out
+# mpiexec -n 8 ./fserial < ./data/testdata2 > ./testdata2_f.out
+
+mpiexec -n 1 ./serial < ./data/actualdata1 > ./actualdata1_s.out
+mpiexec -n 1 ./serial < ./data/actualdata2 > ./actualdata2_s.out
+mpiexec -n 1 ./serial < ./data/actualdata3 > ./actualdata3_s.out
+mpiexec -n 1 ./serial < ./data/actualdata4 > ./actualdata4_s.out
